ef1e16b9fe3d2c32027c9a8030b920660991cba1
Walterb0b <osca0001@stud.ek.dk>
2025-09-10 10:39:50 +0200

    Battleship game

--- src/BattleshipGame.java
+++ src/BattleshipGame.java
@@ -1,4 +1,30 @@
-package PACKAGE_NAME;
-
 public class BattleshipGame {
+    public static void main(String[] args) {
+        //Welcome message
+        //While loop til at starte spillet
+        //Placer skibet
+        //Kald checkHit
+
+        System.out.println("==Welcome Captain!==");
+        System.out.println("The enemy ship is hiding in positions 0-6!");
+        System.out.println("Sink it by hitting all 3 cells!");
+
+        //battleshipGame();
+
+    }
+
+    public battleshipGame(){
+        boolean finished = false;
+
+        while(!finished){
+            GameHelper.userInput();
+            Ship.checkHit();
+
+            if(Ship.isSunk){
+                finished = false;
+            }else{
+                finished = true;
+            }
+        }
+    }
 }
