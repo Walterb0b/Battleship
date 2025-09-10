800f765316ac3285db4d3f851a2e0de252e98252
aishadirie <aish0078@stud.ek.dk>
2025-09-10 10:37:41 +0200

    Userinput

--- src/GameHelper.java
+++ src/GameHelper.java
@@ -1,4 +1,23 @@
-package PACKAGE_NAME;
+import java.io.InputStream;
+import java.util.Scanner;
 
-public class GameHelper {
-}
+public class GameHelper{
+    public static void userInput(){
+        Scanner scanner = new Scanner(System.in);
+
+
+
+        System.out.print("Enter your shot");
+
+        //int input
+
+        int input  = scanner.nextInt();
+
+
+    }
+
+
+
+    }
+
+
